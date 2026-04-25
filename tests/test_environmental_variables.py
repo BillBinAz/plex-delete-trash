463196dff6f5de8d49c2e33cc969e51e725a88be
BillB <github@billbennett.org>
2026-04-24 18:35:22 -0700

    Added Test Harness

---
+++ tests/test_environmental_variables.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+
+class TestSensorPush(unittest.TestCase):
+
+    # test get_weather raising exception
+
+    def test_place_holder(self):
+
+        # Test exception
+        assert True
+
+    if __name__ == '__main__':
+        unittest.main()
